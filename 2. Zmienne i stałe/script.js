f4bb426b571058712dd5c4a6eaa61759998f70fb
Marcin Dziewiecki <dziewieckimarcin@gmail.com>
2021-01-24 00:30:35 +0100

    Tasks 2. Zmienne i stałe - structure fix

--- 2. Zmienne i stałe/script.js
+++ 2. Zmienne i stałe/script.js
@@ -1,28 +1,44 @@
 // Zadanie 1
 
-let variableNumber = 123;
-let variableString = "Testing text";
-let varibleCombineNumberAndString = 2 + "dwa";
-let varibleBool = true;
-let variableSpecial = null;
+console.log("");
+console.log("Zadanie 1");
+console.log("");
 
-console.log(variableNumber);
-console.log(variableString);
-console.log(varibleCombineNumberAndString);
-console.log(varibleBool);
-console.log(variableSpecial);
+{
+    let variableNumber = 123;
+    let variableString = "Testing text";
+    let varibleCombineNumberAndString = 2 + "dwa";
+    let varibleBool = true;
+    let variableSpecial = null;
 
+    console.log(variableNumber);
+    console.log(variableString);
+    console.log(varibleCombineNumberAndString);
+    console.log(varibleBool);
+    console.log(variableSpecial);
+}
 
 // Zadanie 2
 
-let number1 = 5;
-let number2 = 19;
-let sum = 0;
+console.log("");
+console.log("Zadanie 2");
+console.log("");
 
-console.log(sum = number1 + number2);
+{
+    let number1 = 5;
+    let number2 = 19;
+    let sum = 0;
 
+    console.log(sum = number1 + number2);
+}
 
 // Zadanie 3
 
-let variable;
-console.log(variable);          // wynik to: undefined - poniewa¿ nie przypisaliœmy nic do zmiennej. Nie wiadomo jakiej jest wartoœci oraz jakiego jest typu.
+console.log("");
+console.log("Zadanie 3");
+console.log("");
+
+{
+    let variable;
+    console.log(variable);          // wynik to: undefined - poniewa¿ nie przypisaliœmy nic do zmiennej. Nie wiadomo jakiej jest wartoœci oraz jakiego jest typu.
+}
