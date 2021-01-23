9b2d7af0b96b12626d9b1b71a663448590ce5488
Marcin Dziewiecki <dziewieckimarcin@gmail.com>
2021-01-23 23:20:46 +0100

    Tasks 1.Początki completed

---
+++ 1.Początki/script.js
@@ -0,0 +1,18 @@
+// Zadanie  1
+
+console.log("Marcin");
+
+
+// Zadanie 2
+
+12 + 32;    // wynik to: 44
+52 / 16;    // wynik to: 3.25
+5 * 2;      // wynik to: 10
+
+
+// Zadanie 3
+
+"Hello World"       // wynik to: "Hello World"
+Hello World;        // wynik to: Uncaught SyntaxError: Unexpected identifier
+"Hello World;
+                    // wynik to: Uncaught SyntaxError: Invalid or unexpected token
