c45a76f98a8af9dcb96f66866af57bd22c89404e
Marcin Dziewiecki <dziewieckimarcin@gmail.com>
2021-01-23 23:51:56 +0100

    Tasks 2. Zmienne i stałe completed

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,28 @@
+// Zadanie 1
+
+let variableNumber = 123;
+let variableString = "Testing text";
+let varibleCombineNumberAndString = 2 + "dwa";
+let varibleBool = true;
+let variableSpecial = null;
+
+console.log(variableNumber);
+console.log(variableString);
+console.log(varibleCombineNumberAndString);
+console.log(varibleBool);
+console.log(variableSpecial);
+
+
+// Zadanie 2
+
+let number1 = 5;
+let number2 = 19;
+let sum = 0;
+
+console.log(sum = number1 + number2);
+
+
+// Zadanie 3
+
+let variable;
+console.log(variable);          // wynik to: undefined - poniewa¿ nie przypisaliœmy nic do zmiennej. Nie wiadomo jakiej jest wartoœci oraz jakiego jest typu.
